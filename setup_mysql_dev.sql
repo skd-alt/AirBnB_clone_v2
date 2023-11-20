1661963167d19c364e687bff06bf36a96b9d24d9
skd-alt <sipho@skanalytics.co.za>
2023-11-20 09:56:30 -0800

    MySQL Setup

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- Creates new database hbnb_dev_db new user hbnb_dev and priveleges
+CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
