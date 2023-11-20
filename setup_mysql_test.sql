9b8b318a9ca3745db48d30098416eef5c067f6f8
skd-alt <sipho@skanalytics.co.za>
2023-11-20 18:13:51 +0000

    MySQL Setup for test database

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- Creates new test database hbnb_test_db new user hbnb_test and priveleges
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
